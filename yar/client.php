bf203339b7a2ccf9d604d9abd7ef5faa3ac01c78
caizixin <caizixinmail@gmail.com>
2016-08-11 18:20:04 +0800

    可执行demo

--- yar/client.php
+++ yar/client.php
@@ -5,5 +5,6 @@
  * Date: 16/8/11
  * Time: 下午5:52
  */
-$client = new Yar_Client('http://182.92.170.3:9990/server.php/api');
-$reuslt = $client->api('caizixin');
+$client = new Yar_Client('http://182.92.170.3:9990');
+$result = $client->test('caizixin');
+var_dump($result);
