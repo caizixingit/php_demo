9e56147727fe99345c9954e20d0f4b97a33321a3
caizixin <caizixinmail@gmail.com>
2016-08-11 17:54:16 +0800

    server client demo

---
+++ yar/client.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caizixin
+ * Date: 16/8/11
+ * Time: 下午5:52
+ */
+$client = new Yar_Client('http://182.92.170.3:9990/server.php/api');
+$reuslt = $client->api('caizixin');
--- yar/server.php
+++ yar/server.php
@@ -2,13 +2,9 @@
 
 class API
 {
-	/**
-	 * the doc info will be generated automatically into service info page.
-	 * @params
-	 * @return
-	 */
-	public function api($parameter, $option = "foo")
+	public function api($name, $option = "for")
 	{
+		return $name. ' '. $option. 'thy';
 	}
 
 	protected function client_can_not_see()
