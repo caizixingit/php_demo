0f6e9c5a5a9f883797a91acbb303894af9e70102
caizixin <caizixinmail@gmail.com>
2016-08-11 17:12:37 +0800

    server demo

--- yar/server.php
+++ yar/server.php
@@ -1,5 +1,20 @@
 <?php
+
 class API
 {
-	public function api
+	/**
+	 * the doc info will be generated automatically into service info page.
+	 * @params
+	 * @return
+	 */
+	public function api($parameter, $option = "foo")
+	{
+	}
+
+	protected function client_can_not_see()
+	{
+	}
 }
+
+$service = new Yar_Server(new API());
+$service->handle();
